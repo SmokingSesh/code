0fd71186c4544cc7eb59624f0ea373311db905dd
SmokingSesh <153219620+SmokingSesh@users.noreply.github.com>
2026-04-22 12:46:30 +0300

    Update signer.go

--- signer/signer.go
+++ signer/signer.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"slices"
 	"sort"
 	"strconv"
 	"strings"
@@ -16,54 +15,63 @@ func main() {
 var md5Mutex sync.Mutex
 
 func SingleHash(in, out chan interface{}) {
-
+	var wg sync.WaitGroup
 	for data := range in {
-		str := data.(string)
-		var crcData, crcMd5 string
-		var wg sync.WaitGroup{}
-		wg.Add(2)
-		go func() {
+		wg.Add(1)
+		go func(data interface{}) {
 			defer wg.Done()
-			crcData = DataSignerCrc32(str)
-		}()
+			str := fmt.Sprint(data)
+			var crcData, crcMd5 string
+			var wg2 sync.WaitGroup
+			wg2.Add(2)
+			go func() {
+				defer wg2.Done()
+				crcData = DataSignerCrc32(str)
+			}()
 
-		go func() {
-			defer wg.Done()
-			md5Mutex.Lock()
-			md5Data := DataSignerMd5(str)
-			md5Mutex.Unlock()
-			crcMd5 = DataSignerCrc32(md5Data)
-		}()
-		wg.Wait()
+			go func() {
+				defer wg2.Done()
+				md5Mutex.Lock()
+				md5Data := DataSignerMd5(str)
+				md5Mutex.Unlock()
+				crcMd5 = DataSignerCrc32(md5Data)
+			}()
+			wg2.Wait()
 
-		out <- crcData + "~" + crcMd5
+			out <- crcData + "~" + crcMd5
+		}(data)
+
 	}
-
+	wg.Wait()
 }
 
 func MultiHash(in, out chan interface{}) {
-
+	var wg sync.WaitGroup
 	for data := range in {
-		h := data.(string)
-		m := make([]string, 6)
-		var wg sync.WaitGroup
-		wg.Add(6)
-		for th := 0; th < 6; th++ {
-			th := th
-			go func () {
-				defer wg.Done()
-				predRes := strconv.Itoa(th) + h
-				m[th] = DataSignerCrc32(predRes)
-			}()
-		}
-		wg.Wait()
-		var n string
-		for _, finRes := range m {
-			n += finRes
-		}
-		out <- n
+		wg.Add(1)
+		go func(data interface{}) {
+			defer wg.Done()
+			h := data.(string)
+			m := make([]string, 6)
+			var wg2 sync.WaitGroup
+			wg2.Add(6)
+			for th := 0; th < 6; th++ {
+				th := th
+				go func() {
+					defer wg2.Done()
+					predRes := strconv.Itoa(th) + h
+					m[th] = DataSignerCrc32(predRes)
+				}()
+			}
+			wg2.Wait()
+			var result string
+			for _, part := range m {
+				result += part
+			}
+			out <- result
+		}(data)
 	}
-
+	wg.Wait()
 }
 
 func CombineResults(in, out chan interface{}) {
@@ -81,9 +89,9 @@ func ExecutePipeline(jobs ...job) {
 	var in chan interface{}
 
 	for _, j := range jobs {
-		out := make(chan interface{})
+		out := make(chan interface{}, 10)
 		wg.Add(1)
-		go func (worker job, in, out chan interface{}){
+		go func(worker job, in, out chan interface{}) {
 			defer wg.Done()
 			worker(in, out)
 			close(out)
@@ -92,4 +100,3 @@ func ExecutePipeline(jobs ...job) {
 	}
 	wg.Wait()
 }
-
